2054aef773a80d273c72e653557a7acd414128b8
Hook25 <massigi97@gmail.com>
2017-03-09 21:21:23 +0100

    prova

--- cerchietti.py
+++ cerchietti.py
@@ -1,11 +1,11 @@
-from pygame import *
-import __builtin__ as bi
-"""
-new module structure, button_action is called on key pressed
-"""
-def button_action():
-  global event_buffer
-  
-def someshit():
-  bi.append_to_bg([image.load("img/download.gif"),10,10])
-  print "shit happend"
+from pygame import Surface
+space_to_circle_rateo = 25/9 #a circle is 25/9 of space
+total_size = 100
+def get_to_blit(scale):
+  global space_to_circle_rateo
+  global total_size
+  size = total_size*scale
+  space = space_to_circle_rateo * scale
+  total_width = (4*(6*space)) + (4*(5*(6*space))) # a circle is 6 times a space
+  for i in range(20):
+    
